c440d9eb906eb597497485585e3a84b3aa673b5f
Pablo <pabloconejerosoriano@icloud.com>
2026-04-23 16:23:07 +0200

    feat(location): added LocationProvider to consume hooks

--- App.tsx
+++ App.tsx
@@ -1,10 +1,12 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { StatusBar } from 'expo-status-bar';
 import TabNavigator from './src/navigation/TabNavigator';
+import LocationProvider from './src/providers/LocationProvider';
 
 export default function App() {
   return (
     <NavigationContainer>
+      <LocationProvider/>
       <StatusBar style="auto" />
       <TabNavigator />
     </NavigationContainer>
